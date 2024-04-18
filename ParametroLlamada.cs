64e4e97e26cf55050ee25556b1198214231048fe
César A. V. Rodríguez <cesar.av.rodriguez@gmail.com>
2024-04-18 13:06:37 -0600

    Agregar archivos de proyecto.

---
+++ ParametroLlamada.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaCFDI.Utils
+{
+    public class ParametroLlamada
+    {
+        public ParametroLlamada(string nombre, string descripcion, string? valor = null)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Valor = valor;
+        }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public string? Valor { get; set; }
+
+
+        public string QueryParam => $"{Nombre}={Valor}";
+
+        public static string ToQueryString(List<ParametroLlamada> entradas)
+        {
+            // modified from annaka answer in https://stackoverflow.com/questions/829080/how-to-build-a-query-string-for-a-url-in-c
+            var array = entradas.Select(pl => pl.QueryParam).ToArray();
+            return "?" + string.Join("&", array);
+        }
+
+        public static string GetExpresionImpresa()
+        {
+            try
+            {
+                List<ParametroLlamada> entradas = new List<ParametroLlamada>()
+            {
+                { new ParametroLlamada("re", "RFC de emisor", "") },
+                { new ParametroLlamada("rr", "RFC de receptor", "") },
+                { new ParametroLlamada("tt", "total de factura", "") }, // ej: 27240.00
+                { new ParametroLlamada("id", "UUID", "") }
+            };
+
+                // get required values
+                foreach (ParametroLlamada pl in entradas)
+                {
+                    if (string.IsNullOrEmpty(pl.Valor))
+                    {
+                        Console.Write($"Capture por favor el {pl.Descripcion}: ");
+                        pl.Valor = Console.ReadLine();
+                    }
+                }
+
+                return ToQueryString(entradas);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using SAT_Service;
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Este proyecto se creó con los iguientes pasos:
+//
+// 1. Descargar el archivo de definición "" del sat
+
+ConsultaCFDIServiceClient client = new ConsultaCFDIServiceClient();
+
+string expresionImpresa = ConsultaCFDI.Utils.ParametroLlamada.GetExpresionImpresa();
+
+// esta función la genera VS al importar la referencia a partir del archivo consultacfdiservice.wsdl
+Acuse res = client.Consulta(expresionImpresa);
+
+Console.WriteLine($"Estado: {res.Estado}");
+Console.WriteLine($"Cancelable: {res.EsCancelable}");
+if (!string.IsNullOrEmpty(res.EstatusCancelacion))
+{
+    Console.WriteLine($"Estado de cancelación: {res.EstatusCancelacion}");
+}
+Console.WriteLine($"Validación: {res.ValidacionEFOS}");
